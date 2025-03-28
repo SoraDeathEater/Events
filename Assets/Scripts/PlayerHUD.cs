1cd2398951f1230835b4b2b64d8ba7216adeb20f
Sora Miura <soradeatheater23@gmail.com>
2025-03-27 23:11:13 -0500

    Committing?
    Im not even sure if this is even working anymore...

---
+++ Assets/Scripts/PlayerHUD.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerHUD : MonoBehaviour
+{
+    [SerializeField] Image healthBar;
+    [SerializeField] TMP_Text currentAmmoText;
+    [SerializeField] TMP_Text maxAmmoText;
+    [SerializeField] Gun gun;
+
+    FPSController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<FPSController>();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
